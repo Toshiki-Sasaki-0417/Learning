4a087bf32d1f2763787d7b9a1c3d39e0c38c3df7
Toshiki-Sasaki-0417 <56709901+Toshiki-Sasaki-0417@users.noreply.github.com>
2019-11-04 17:30:13 +0900

    add

---
+++ techgym_python/2-1.py
@@ -0,0 +1,9 @@
+import random
+
+print('違う漢字の番号(例:A1)を入力してください')
+
+level = 1
+print('レベル:' + str(level))
+
+choice = input('(例:A1)')
+print('デバッグ:choice = ' + choice)
---
+++ techgym_python/2-3.py
@@ -0,0 +1,27 @@
+#2-3
+
+import random
+
+#メッセージ定義
+message_01 = '違う漢字の番号(例:A1)を入力してください'
+message_02 = 'レベル:'
+message_03 = '(例:A1)'
+message_04 = 'デバッグ:choice = '
+
+#
+level = '1'
+
+#関数定義
+def start_message():
+    print(message_01)
+
+def section_message():
+    print(message_02 + level)
+
+def play():
+    section_message()
+    choice = input(message_03)
+    print(message_04 + choice)
+
+start_message()
+play()
