725195233b10d17340fb20edc7e778d846ccf460
Toshiki-Sasaki-0417 <56709901+Toshiki-Sasaki-0417@users.noreply.github.com>
2019-11-04 18:05:08 +0900

    add

--- techgym_python/2-5.py
+++ techgym_python/2-5.py
@@ -1,4 +1,4 @@
-#2-4
+#2-5
 
 import random
 
@@ -9,7 +9,7 @@ message_03 = '(例:A1)'
 message_04 = 'デバッグ:choice = '
 
 #
-data = [['貝','貝'],['土','土'],['眼','眼']]
+data = [['見','貝'],['土','士'],['眠','眼']]
 level = '1'
 int_randam = random.randint(0,2)
 print(int_randam)
@@ -25,17 +25,25 @@ def view_question():
     question_word = data[int_randam]
     print(question_word)
     
-    #ループ用変数初期化
+    #変数
     i = 0
     j = 0
+    k = 0
     question_words = ''
+    randam_idx = random.randint(0,8)
 
     while i < 3:
         i += 1
         while j < 3:
-            question_words += question_word[0]
+            if randam_idx == k:
+                question_words += question_word[1]
+            else:
+                question_words += question_word[0]
             j += 1
+            k += 1
         print(question_words)
+        question_words = ''
+        j = 0
 
 def play():
     section_message()
